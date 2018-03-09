d77b04791877e9d2998779164931cdc547c7ff1f
gurukathir-15 <kathir.jawahar@anywhere.co>
2018-03-09 09:51:29 +0530

    Initial commit

---
+++ app/src/main/java/com/guru/inviter/constants/Contants.java
@@ -0,0 +1,8 @@
+package com.guru.inviter.constants;
+
+/**
+ * Created by Guru on 09-03-2018.
+ */
+
+public class Contants {
+}
---
+++ app/src/main/java/com/guru/inviter/model/InviteDetails.java
@@ -0,0 +1,8 @@
+package com.guru.inviter.model;
+
+/**
+ * Created by Guru on 09-03-2018.
+ */
+
+public class InviteDetails {
+}
